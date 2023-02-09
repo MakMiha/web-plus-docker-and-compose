1adb4d902bc4db8bc5a36bd4e54a3e0e750e9eda
MakMiha <mackarowmixail@yandex.ru>
2023-02-09 13:57:28 +0300

    fix cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,9 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors({ 
-    origin: ['https://kpd-makarov.nomoredomainsclub.ru', 'http://kpd-makarov.nomoredomainsclub.ru'],
-  });
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
